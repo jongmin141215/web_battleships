2741c921020e782e34c8227405208d72482924a5
Jongmin Kim <kjm141215@gmail.com>
2015-09-15 10:55:20 +0100

    added what is your name?

--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
 feature 'Starting a new game' do
-  scenario 'I am asked to enter my name' do
+  scenario 'There is a link to a new game' do
     visit '/'
     click_link 'New Game'
     expect(page).to have_content "What's your name?"
   end
-end
 
+  scenario 'There is a form to enter your name' do
+    visit '/new_game'
+    fill_in('name', with: 'Jongmin')
+    click_button 'submit'
+  end
+
+
+end
